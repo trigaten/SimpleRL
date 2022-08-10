3040aebf7b640e5374d259b52263067dd6a26892
trigaten <sanderschulhoff@gmail.com>
2022-08-10 11:28:53 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -20,7 +20,5 @@ setuptools.setup(
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.9",
-    install_requires=[
-        "torch>=1.11"
-    ],
+
 )
