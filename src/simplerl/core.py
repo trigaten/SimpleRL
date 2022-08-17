c212e3eaa2a6dfe4b6f00e60d946d02d34a88224
trigaten <sanderschulhoff@gmail.com>
2022-08-17 19:38:01 -0400

    feat: training loop

--- src/simplerl/core.py
+++ src/simplerl/core.py
@@ -1,34 +1,37 @@
 from abc import ABC, abstractmethod
 from collections import namedtuple
 
-"""A `Policy` takes in an observation and returns an action or array of log probs for actions."""
+"""A `Policy` takes in an observation and returns an action or array of 
+log probs for actions."""
 class Policy(ABC):
     
     @abstractmethod
     def __call__(x):
         raise Exception("Not Implemented")
 
-"""An `Agent` is an object which 0 or more policies and is responsible for training them."""
+"""An `Agent` is an object which contains 0 or more policies and is 
+responsible for training them."""
 class Agent(ABC):
     
     @abstractmethod
     def __call__(x):
         raise Exception("Not Implemented")
 
-# def MonoTrainer(agent, env):
-#     # "global" state of training
-#     training_done = False
-#     # how many episodes have been completed thus far
-#     episodes_complete = 0
-#     while not training_done:
-#         obs = env.reset()
-#         done = False
-#         while not done:
-#             obs, reward, done, info = agent(obs)
+def train(agent, env, episodes):
+    # "global" state of training
+    training_done = False
+    # how many episodes have been completed thus far
+    episodes_complete = 0
+    while not training_done:
+        obs = env.reset()
+        done = False
+        while not done:
+            next_obs, reward, done, info = agent(obs)
+            
 
-#         episodes_complete+= 1
-#         if episodes_complete > 1000:
-#             training_done = True
+        episodes_complete+= 1
+        if episodes_complete > episodes:
+            training_done = True
 
 
 
